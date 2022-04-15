54b4c746edc8cd71211229c793f4608703b28e83
Jak <janchivariunjin@gmail.com>
2022-04-15 22:12:04 +0800

    added file ex03.js

--- day4/Ex03.js
+++ day4/Ex03.js
@@ -1,4 +1,13 @@
 var http = require("http");
 var fs = require("fs");
+var content = "Free fake API for testing and prototyping.";
 
-http.createServer((request, response) => {});
+fs.writeFileSync("content.html", content);
+http
+  .createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    const data = fs.readFileSync("content.html");
+    response.end(data);
+  })
+  .listen(3002);
+console.log("Server is starting 3002");
