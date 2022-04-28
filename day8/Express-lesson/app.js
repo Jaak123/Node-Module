e4e66f7c1e2c28c2515451a70d6d91d0dc8ecb04
mstars_lab2_03 <mstars_lab2_03@mstars-lab2-03s-iMac.local>
2022-04-28 18:18:47 +0800

    added ex03

--- day8/Express-lesson/app.js
+++ day8/Express-lesson/app.js
@@ -3,29 +3,28 @@ const request = require("request");
 const app = express();
 
 app.get("/api/foods", function (req, res) {
-  request("https://dev-api.mstars.mn/foods", function (err, response, body) {
+  request("https://dev-api.mstars.mn/api/foods", (err, response, body) => {
     if (err) {
       throw err;
     } else {
-      let foods = JSON.parse(body);
       console.log(body);
+      let foods = JSON.parse(body);
       const table1 = `<table>`;
       const table2 = `</table>`;
       const foodData = "";
       foods.data.map((e) => {
-        foodData =
-          foodData +
+        foodData +
           `<tr>
-          <td>${e.name}</td>
-          <td>${e.price}</td>
-          <td>${e.quantity}</td>
-          <td>${e.stock}</td>
-          </tr>`;
+            <td>${e.name}</td>
+            <td>${e.price}</td>
+            <td>${e.quantity}</td>
+            <td>${e.stock}</td>
+            </tr>`;
       });
       let allData = table1 + foodData + table2;
+      res.send(allData);
     }
   });
-  res.send(allData);
 });
 
 app.listen(5000);
