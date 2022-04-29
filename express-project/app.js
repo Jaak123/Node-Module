061ba7558523baf606f44504382226ebd02e460b
mstars_lab2_03 <mstars_lab2_03@mstars-lab2-03s-iMac.local>
2022-04-29 19:41:44 +0800

    added new project

---
+++ express-project/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.get("/:api", function (req, res) {
+  fs.readFile("data.json", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      //   console.log(data);
+      const data1 = JSON.parse(data);
+      const filterData = data1.map((e) => {
+        // console.log(e);
+        if (req.params.api === "age") {
+          //   console.log;
+          return e.age;
+        } else if (req.params.api === "name") {
+          return e.name;
+        } else if (req.params.api === "major") {
+          return e.major;
+        } else if (req.params.api === "profile") {
+          return e;
+        }
+      });
+      res.send(filterData);
+    }
+  });
+});
+
+app.listen(3000);
