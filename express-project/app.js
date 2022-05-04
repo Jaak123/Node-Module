1e0d021c7efeacad8ad8b6d0d93b123c075f0ea4
Jak <janchivariunjin@gmail.com>
2022-05-04 17:59:16 +0800

    added day 11

--- express-project/app.js
+++ express-project/app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Content-Type", "image/jpeg");
+  // res.header("Content-Type", "image/jpeg");
   next();
 });
 
